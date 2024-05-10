fbefd946fe945fcd5c459ace271948a1c9eb17ee
kangcw0107 <kangcw0107@gmail.com>
2024-05-10 17:57:29 +0900

    메인 페이지 레이아웃 수정 - 게시물 받아오기 api 연결 및 게시물 로드 테스트

--- nagne-web-vuejs/src/components/Header/TheHeader.vue
+++ nagne-web-vuejs/src/components/Header/TheHeader.vue
@@ -2,19 +2,13 @@
   <div class="header">
     <div class="navbar">
       <div class="white-space"></div>
-      <div class="navbar-logo" @click="()=>move('main')">
+      <div class="navbar-logo" @click="() => move('main')">
         <div class="left-logo1"></div>
         <div class="left-logo2"></div>
       </div>
       <div class="navbar-icons">
-        <div
-          class="navbar-icons-wrapper"
-        >
-          <font-awesome-icon
-            class="icon"
-            id="navbar-search-btn"
-            :icon="faMagnifyingGlass"
-          />
+        <div class="navbar-icons-wrapper">
+          <font-awesome-icon class="icon" id="navbar-search-btn" :icon="faMagnifyingGlass" />
         </div>
         <div class="navbar-icons-wrapper" id="bell-wrapper">
           <font-awesome-icon class="icon" :icon="faBell" id="navbar-bell" />
@@ -22,48 +16,34 @@
         <div class="navbar-icons-wrapper">
           <font-awesome-icon class="icon" :icon="faMap" id="navbar-map" />
         </div>
-        <div
-          class="navbar-icons-wrapper"
-          @click="toggleUserMenu"
-        >
+        <div class="navbar-icons-wrapper" @click="toggleUserMenu">
           <font-awesome-icon class="icon" :icon="faUser" id="navbar-user" />
         </div>
         <div class="navbar-icons-wrapper" id="navbar-write-icon-wrapper">
-          <font-awesome-icon
-            :icon="faPen"
-            class="icon"
-            id="navbar-write-icon"
-          />
+          <font-awesome-icon :icon="faPen" class="icon" id="navbar-write-icon" />
         </div>
         <div class="navbar-icons-wrapper navbar-write-button-wrapper">
           <div class="navbar-write-button jua-regular-large">작성하기</div>
         </div>
       </div>
-      
-      
+
+
       <!-- phoneSize  -->
       <div class="phone-size-navmenu">
         <div class="navbar-icons-wrapper burger-menu-icon-wrapper">
-          <font-awesome-icon
-            class="icon burger-menu-icon"
-            id="navbar-search-btn"
-            :icon="faMagnifyingGlass"
-          />
+          <font-awesome-icon class="icon burger-menu-icon" id="navbar-search-btn" :icon="faMagnifyingGlass" />
         </div>
-        <div
-          class="navbar-icons-wrapper burger-menu-icon-wrapper"
-          @click="toggleSidebar"
-        >
+        <div class="navbar-icons-wrapper burger-menu-icon-wrapper" @click="toggleSidebar">
           <font-awesome-icon :icon="faBars" class="burger-menu-icon" />
         </div>
       </div>
     </div>
     <!-- phoneSize - SideBar  -->
     <!-- <HeaderSidebar :showSidebar="showSidebar" @updateShowSidebar="toggleSidebar" :class="{ 'off-display': !showSidebar }"/> -->
-    
+
     <!-- UserMenu -->
     <ul v-show="showUserMenu" class="user-menu list-group" ref="userMenu">
-      <li v-show="isLogin" class="list-group-item" @click="()=>move('login')">로그인</li>
+      <li v-show="isLogin" class="list-group-item" @click="() => move('login')">로그인</li>
       <li v-show="!isLogin" class="list-group-item">A second item</li>
       <li v-show="!isLogin" class="list-group-item">A third item</li>
       <li v-show="!isLogin" class="list-group-item">A fourth item</li>
@@ -89,17 +69,21 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 // import HeaderSidebar from "./HeaderSidebar.vue";
 import { ref } from "vue";
-import {useRouter} from "vue-router";
+import { useRouter } from "vue-router";
 const router = useRouter();
 
 const isLogin = ref(true);
 
-const move = (path)=>{
-  if (path==='login') {
+const move = (path, now) => {
+  const moveTo = { name: path };
+  if (path === 'login') {
     showUserMenu.value = !showUserMenu.value;
+    moveTo.query = { from: now, to: path };
   }
-
-  router.push({name:path});
+  //페이지 이동 시 열려있는 메뉴 전부 닫기
+  showUserMenu.value = false;
+  showSidebar.value = false;
+  router.push(moveTo);
 }
 
 const showSidebar = ref(false);
@@ -111,6 +95,7 @@ const showUserMenu = ref(false);
 function toggleUserMenu() {
   showUserMenu.value = !showUserMenu.value;
 }
+
 
 
 </script>
@@ -128,6 +113,7 @@ a {
   justify-content: center;
   align-items: center;
 }
+
 .horizon-border {
   height: 2px;
   width: 100%;
@@ -159,6 +145,7 @@ a {
   background-image: url("@/assets/logo_img.png");
   background-size: cover;
 }
+
 .left-logo2 {
   width: 140px;
   height: 50px;
@@ -174,6 +161,7 @@ a {
   align-items: center;
   gap: 15px;
 }
+
 .navbar-icons-wrapper {
   background-color: white;
   border-radius: 50px;
@@ -188,9 +176,11 @@ a {
 
 .user-menu {
   position: absolute;
-  top: 13%;
-  left: 80%; /* 중앙 정렬을 위한 왼쪽 여백 설정 */
-  transform: translateX(-50%); /* X축 기준 중앙 정렬 */
+  top: 8%;
+  left: 75%;
+  /* 중앙 정렬을 위한 왼쪽 여백 설정 */
+  transform: translateX(-50%);
+  /* X축 기준 중앙 정렬 */
   width: auto;
   z-index: 100;
 }
@@ -206,12 +196,14 @@ a {
   transition: all 100ms;
   cursor: pointer;
   background-color: rgb(118, 189, 255);
+
   .icon {
     color: white;
     scale: 1.1;
     transition: all 200ms;
   }
 }
+
 .navbar-icons-wrapper:active {
   scale: 1.1;
   transition: all 100ms;
@@ -233,9 +225,11 @@ a {
   border-radius: 50px;
   font-size: large;
 }
+
 .navbar-write-button-wrapper:hover {
   color: white;
 }
+
 .navbar-write-button-wrapper:active {
   background-color: #2a79ff;
   color: white;
@@ -259,15 +253,24 @@ a {
 #faArrowUp-button {
   background-color: rgb(118, 189, 255);
   color: white;
-  position: fixed; /* 고정 위치 */
-  bottom: 20px; /* 하단에서 20px 위 */
-  right: 30px; /* 우측에서 30px 왼쪽 */
-  z-index: 99; /* 다른 내용물 위에 보이도록 z-index 설정 */
-  border: none; /* 테두리 없음 */
-  outline: none; /* 외곽선 없음 */
-  cursor: pointer; /* 커서 모양을 손가락 모양으로 */
-  padding: 5px; /* 내부 여백 */
-  border-radius: 50px; /* 테두리 둥글게 */
+  position: fixed;
+  /* 고정 위치 */
+  bottom: 20px;
+  /* 하단에서 20px 위 */
+  right: 30px;
+  /* 우측에서 30px 왼쪽 */
+  z-index: 99;
+  /* 다른 내용물 위에 보이도록 z-index 설정 */
+  border: none;
+  /* 테두리 없음 */
+  outline: none;
+  /* 외곽선 없음 */
+  cursor: pointer;
+  /* 커서 모양을 손가락 모양으로 */
+  padding: 5px;
+  /* 내부 여백 */
+  border-radius: 50px;
+  /* 테두리 둥글게 */
 }
 
 #faArrowUp-button:active {
@@ -296,6 +299,7 @@ a {
   .header {
     width: 100%;
   }
+
   .navbar {
     max-width: 880px;
     width: 80%;
@@ -311,6 +315,7 @@ a {
     width: 100%;
     transition: 0.5s;
   }
+
   .navbar-write-button-wrapper {
     display: none;
   }
@@ -319,12 +324,14 @@ a {
     display: flex;
   }
 }
+
 @media screen and (max-width: 640px) {
 
   .navbar {
     width: 100%;
     transition: 0.5s;
   }
+
   .navbar-icons {
     display: none;
     transition: all 0.5s;
---
+++ nagne-web-vuejs/src/components/MainPage/TheArticle.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="article-wrapper">
+    <div class="article">
+      <img src="@/assets/logo.png" />
+      <p>{{ article.content }}</p>
+      <p>좋아요 : {{ article.likeCount }}</p>
+      <p>북마크 : {{ article.isBookmarked }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  article: Object,
+})
+</script>
+
+<style scoped>
+.article-wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 20px 0 20px 0;
+}
+
+.article {
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 15px;
+  border-bottom: 1.5px solid rgb(228, 228, 228);
+}
+
+.article img {
+  justify-content: center;
+  border-radius: 25px;
+  background-color: rgb(194, 194, 194);
+  margin-bottom: 30px;
+
+}
+</style>
