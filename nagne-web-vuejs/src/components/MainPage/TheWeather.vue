2e88f06e29daea92ab3404b91e944b030fc8848c
monam2 <kangcw0107@gmail.com>
2024-05-11 21:53:38 +0900

    메인 페이지 / 사이드 바 내 각 컴포넌트 생성

--- nagne-web-vuejs/src/components/MainPage/TheWeather.vue
+++ nagne-web-vuejs/src/components/MainPage/TheWeather.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="side-weather">
-    <img :src="userWeather.icon" :width="100" alt="">
-    <p>{{ userWeather.temp }} °C</p>
-    <p>{{ userWeather.humidity }} %</p>
-    
+    <img v-if="!isWeatherLoding" class="weather-icon" :src="userWeather.icon" alt="">
+    <img v-else class="weather-loading-spinner" src="@/assets/blue_spinner.svg" alt="">
+    <div class="weather-text jua-regular-large">
+    <p v-if="!isWeatherLoding">기온 : {{ userWeather.temp }} °C</p>
+    <p v-if="!isWeatherLoding">습도 : {{ userWeather.humidity }} %</p>
+  </div>
   </div>
 </template>
 
@@ -14,6 +16,7 @@ import {weather_icons} from '@/assets/weather_icon/weather_icons.js';
 
 const userLocation = ref({});
 const userWeather = ref({});
+const isWeatherLoding = ref(true);
 
 const getUserLocaWeather = async ()=>{
   await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.value.lat}&lon=${userLocation.value.lon}&units=metric&appid=${import.meta.env.VITE_WEATHER_API_KEY}`)
@@ -26,12 +29,13 @@ const getUserLocaWeather = async ()=>{
         condition : data.weather[0].description,
         icon : weather_icons[data.weather[0].icon],
       }
-      console.log(userWeather.value)
+      isWeatherLoding.value = false;
     })
     .catch(error => {
     });
 }
 onMounted(async() => {
+  isWeatherLoding.value = true;
   navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
@@ -50,18 +54,28 @@ onMounted(async() => {
   align-items: center;
   justify-content: space-around;
   padding: 5px 30px 5px 30px;
+  margin-bottom: 25px;
+}
+
+.weather-loading-spinner {
+  width: 100px;
+  height: 100px;
+  margin-left: 55px;
 }
 
 .weather-text {
   display: flex;
   flex-direction: column;
-  gap: 5px;
   color: #5b5c5c;
+  gap: 10px;
+}
+
+.weather-text p{
+  margin: 0px;
 }
 
 .weather-icon {
   width: 100px;
   height: 100px;
-  border-radius: 15px;
 }
 </style>
