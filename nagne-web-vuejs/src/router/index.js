d3e4b9190f699f7859b90179a0626ceebacdda72
monam2 <kangcw0107@gmail.com>
2024-05-12 02:05:55 +0900

    게시글 작성 페이지 경로 추가

--- nagne-web-vuejs/src/router/index.js
+++ nagne-web-vuejs/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "@/pages/MainPage.vue";
 import LoginPage from "@/pages/LoginPage.vue";
 import SignupPage from "@/pages/SignupPage.vue";
-
+import ArticleWritePage from "@/pages/ArticleWritePage.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -20,6 +20,11 @@ const router = createRouter({
       path: "/signup",
       name: "signup",
       component: SignupPage,
+    },
+    {
+      path: "/articles/write",
+      name: "write",
+      component: ArticleWritePage
     },
   ],
 });
