6b75b28cb3bce674c8e37ed54c97a0438d385bb2
kangcw0107 <kangcw0107@gmail.com>
2024-05-13 17:41:41 +0900

    Chore : Pinia 설치 및 main.js에 import

--- nagne-web-vuejs/src/main.js
+++ nagne-web-vuejs/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
@@ -8,6 +9,8 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
+const pinia = createPinia();
+
 library.add(fas);
 
-createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+createApp(App).use(pinia).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
