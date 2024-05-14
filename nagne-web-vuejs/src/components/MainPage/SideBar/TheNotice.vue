1a9caaf6837a934ae39733a768beaa5a7b1c28b5
kangcw0107 <kangcw0107@gmail.com>
2024-05-14 09:41:26 +0900

    Rename : 메인페이지 사이드바 관련 컴포넌트 분류

---
+++ nagne-web-vuejs/src/components/MainPage/SideBar/TheNotice.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="side-notice">
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.side-notice {
+  width: 100%;
+  height: 160px;
+  background-color: #a6afab;
+}
+</style>
