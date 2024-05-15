5c547327f98870d65a7fd13d0fc070b2974cfd62
kangcw <kangcw0107@gmail.com>
2024-05-15 09:35:05 +0900

    Feat : 게시물 작성 페이지 라우트 추가

--- nagne-web-vuejs/src/router/index.js
+++ nagne-web-vuejs/src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "@/pages/MainPage.vue";
-import LoginPage from "@/pages/LoginPage.vue";
+import LoginPage from "@/pages/Login/LoginPage.vue";
 import SignupPage from "@/pages/SignupPage.vue";
-import ArticleWritePage from "@/pages/ArticleWritePage.vue";
-import LogoutPage from "@/pages/LogoutPage.vue";
+import LogoutPage from "@/pages/Login/LogoutPage.vue";
+import WritePage from "@/pages/write/WritePage.vue";
+import PlanWritePage from "@/pages/write/PlanWrite/PlanWritePage.vue";
+import ArticleWritePage1 from "@/pages/write/ArticleWrite/ArticleWritePage1.vue";
+import ArticleWritePage2 from "@/pages/write/ArticleWrite/ArticleWritePage2.vue";
+import ArticleWritePage3 from "@/pages/write/ArticleWrite/ArticleWritePage3.vue";
+import ArticleWritePage4 from "@/pages/write/ArticleWrite/ArticleWritePage4.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -28,9 +33,24 @@ const router = createRouter({
       component: SignupPage,
     },
     {
-      path: "/articles/write",
+      path: "/write",
       name: "write",
-      component: ArticleWritePage,
+      component: WritePage,
+    },
+    {
+      path: "/articles/write",
+      name: "articleWrite",
+      children : [
+        {path: "1", name: 'articleWrite1', component: ArticleWritePage1},
+        {path: "2", name: 'articleWrite2', component: ArticleWritePage2},
+        {path: "3", name: 'articleWrite3', component: ArticleWritePage3},
+        {path: "4", name: 'articleWrite4', component: ArticleWritePage4},
+      ]
+    },
+    {
+      path: "/plans/write",
+      name: "planWrite",
+      component: PlanWritePage,
     },
   ],
 });
