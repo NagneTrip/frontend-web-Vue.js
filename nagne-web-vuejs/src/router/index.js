9155d66ffa59de5a9b877636f7b837a4288ffad5
kangcw <kangcw0107@gmail.com>
2024-05-16 01:34:37 +0900

    Fix : article wrte 관련 페이지를 라우터가 아닌 컴포넌트(탭)으로 구성

--- nagne-web-vuejs/src/router/index.js
+++ nagne-web-vuejs/src/router/index.js
@@ -5,10 +5,7 @@ import SignupPage from "@/pages/SignupPage.vue";
 import LogoutPage from "@/pages/Login/LogoutPage.vue";
 import WritePage from "@/pages/write/WritePage.vue";
 import PlanWritePage from "@/pages/write/PlanWrite/PlanWritePage.vue";
-import ArticleWritePage1 from "@/pages/write/ArticleWrite/ArticleWritePage1.vue";
-import ArticleWritePage2 from "@/pages/write/ArticleWrite/ArticleWritePage2.vue";
-import ArticleWritePage3 from "@/pages/write/ArticleWrite/ArticleWritePage3.vue";
-import ArticleWritePage4 from "@/pages/write/ArticleWrite/ArticleWritePage4.vue";
+import ArticleWritePage from "@/pages/write/ArticleWrite/ArticleWritePage.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -40,12 +37,7 @@ const router = createRouter({
     {
       path: "/articles/write",
       name: "articleWrite",
-      children : [
-        {path: "1", name: 'articleWrite1', component: ArticleWritePage1},
-        {path: "2", name: 'articleWrite2', component: ArticleWritePage2},
-        {path: "3", name: 'articleWrite3', component: ArticleWritePage3},
-        {path: "4", name: 'articleWrite4', component: ArticleWritePage4},
-      ]
+      component: ArticleWritePage
     },
     {
       path: "/plans/write",
