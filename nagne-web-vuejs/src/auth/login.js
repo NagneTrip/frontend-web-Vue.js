0b9f5725b4ab0574c35137c9521764fd442687ec
kangcw0107 <kangcw0107@gmail.com>
2024-05-17 15:08:38 +0900

    Style : 로그인, 회원가입 관련 불필요한 코드 정리

--- nagne-web-vuejs/src/auth/login.js
+++ nagne-web-vuejs/src/auth/login.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const login = async (username, password, check) => {
+const login = async (username, password) => {
   return await axios
     .post("http://localhost:8080/api/users/login", {
       username: username,
--- nagne-web-vuejs/src/auth/signup.js
+++ nagne-web-vuejs/src/auth/signup.js
@@ -9,7 +9,7 @@ const signup = (user) => {
     })
     .then(({ data }) => {
       if (data.success) {
-        alert("회원가입에 성공했습니다. 잠시후 메인 화면으로 이동합니다.");
+        alert("회원가입에 성공했습니다. 로그인 후 이용하실 수 있습니다.");
       }
     })
     .catch((err) => alert("회원가입 실패"));
