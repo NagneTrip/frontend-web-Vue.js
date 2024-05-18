2bc7e7c13d51549f5d8dd8eff28ab991d184aa75
kangcw <kangcw0107@gmail.com>
2024-05-18 20:01:18 +0900

    Feat : 지도로 찾기 페이지 - 카카오맵 api 추가

---
+++ nagne-web-vuejs/src/pages/map/FindByMapPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <KakaoMap class="map" :lat="coordinate.lat" :lng="coordinate.lng" :draggable="true"
+  :width="'100%'" :height="'89vh'">
+    <KakaoMapMarker :lat="coordinate.lat" :lng="coordinate.lng"></KakaoMapMarker>
+  </KakaoMap>
+
+</template>
+
+<script setup>
+import { KakaoMap, KakaoMapMarker } from 'vue3-kakao-maps';
+const coordinate = {
+  lat: 37.566826,
+  lng: 126.9786567
+};
+</script>
+
+<style scoped></style>
