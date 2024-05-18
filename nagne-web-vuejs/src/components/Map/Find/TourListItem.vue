c44dfc063980bb90e6ce5fdc674515ab6a6aa74a
kangcw <kangcw0107@gmail.com>
2024-05-19 04:02:42 +0900

    Feat : 현재 위치에서 찾기 - 관광지 리스트 보여줄 컴포넌트 생성 및 추가

---
+++ nagne-web-vuejs/src/components/Map/Find/TourListItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="list-item">
+        <div class="item-img">
+            <img src="/src/assets/logo/logo.png" :width="100" :height="100" alt="">
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.list-item {
+    color: black;
+    background-color: rgb(211, 211, 211);
+    width: 95%;
+    height: 250px;
+    border-radius: 25px;
+    margin: 0 auto 20px auto;
+    padding: 20px;
+    box-shadow: 3px 4px 5px 0px rgba(0, 0, 0, 0.4);
+}
+</style>
