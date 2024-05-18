b0fadb46d683089d6a86157189be4625851b6b9b
kangcw <kangcw0107@gmail.com>
2024-05-18 20:02:20 +0900

    Chore : npm i vue3-kakao-maps

--- nagne-web-vuejs/src/main.js
+++ nagne-web-vuejs/src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import { useKakao } from 'vue3-kakao-maps/@utils';
 
 // fontawesome import
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -12,6 +13,6 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 const pinia = createPinia();
 
 library.add(fas);
+useKakao(import.meta.env.VITE_KAKAO_MAP_KEY);
 
 createApp(App).use(pinia).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
-window.Kakao.init(import.meta.env.VITE_KAKAO_JS_API_KEY);
