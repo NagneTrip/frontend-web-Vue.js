7fe3dcda2b313ad65dd9876a9a0220b16d5f6727
kangcw <kangcw0107@gmail.com>
2024-05-20 00:32:37 +0900

    Feat : 유저 정보 페이지 - 팔로잉/팔로우 목록에서 팔로우/취소 API 연동

--- nagne-web-vuejs/src/components/User/FollowListItem.vue
+++ nagne-web-vuejs/src/components/User/FollowListItem.vue
@@ -10,17 +10,59 @@
         </div>
         
         </div>
-        <button class="follow-btn jua-regular">팔로우</button>
-        <!-- <button class="tab jua-regular">팔로잉</button> -->
+        <button v-if="!isFollow" class="follow-btn jua-regular" @click="follow">팔로우</button>
+        <button v-if="isFollow" class="unfollow jua-regular" @click="unfollow">팔로잉</button>
     </div>
 </template>
 
 <script setup>
+import {ref, onMounted} from 'vue';
+import axios from "axios";
+import { useAuthStore } from "@/store/auth";
+const authStore = useAuthStore();
 
 const props = defineProps({
     followItem : Object,
 })
-console.log(props.followItem)
+
+const isFollow = ref(false);
+
+onMounted(async ()=> {
+    if (!sessionStorage.getItem('token') || !authStore.isAuthenticated) {
+        return;
+    }
+
+    await axios.get(`http://localhost:8080/api/follow/${props.followItem.id}`, {
+        headers : {Authorization: `Bearer ${sessionStorage.getItem('token')}`, }
+    }).then(({data})=>{
+        isFollow.value = data.response.checkFollow;
+    })
+})
+
+const follow = ()=> {
+    if (!sessionStorage.getItem('token') || !authStore.isAuthenticated) {
+        alert('에러 발생! 로그인을 다시 진행하세요!')
+        return;
+    }
+
+    axios.post(`http://localhost:8080/api/follow`, {'followId':props.followItem.id},{
+        headers : {Authorization: `Bearer ${sessionStorage.getItem('token')}`, }
+    }).then(({data})=>{
+        isFollow.value = true;
+    })
+}
+const unfollow = async ()=> {
+    if (!sessionStorage.getItem('token') || !authStore.isAuthenticated) {
+        alert('에러 발생! 로그인을 다시 진행하세요!')
+        return;
+    }
+
+    axios.delete(`http://localhost:8080/api/follow/${props.followItem.id}`, {
+        headers : {Authorization: `Bearer ${sessionStorage.getItem('token')}`, }
+    }).then(()=>{
+        isFollow.value = false;
+    })
+}
 </script>
 
 <style scoped>
@@ -59,6 +101,22 @@ p {
     background-color: rgb(118, 189, 255);
     border-radius: 8px;
     color: white;
+    transition: 0.2s all;
+}
+
+.unfollow {
+    width: 65px;
+    height: 25px;
+    border: none;
+    background-color: rgb(167, 167, 167);
+    border-radius: 8px;
+    color: white;
+    transition: 0.2s all;
+}
+
+.unfollow:hover {
+    background-color: #f36f57;
+    scale: 1.1;
     transition: 0.2s all;
 }
 
