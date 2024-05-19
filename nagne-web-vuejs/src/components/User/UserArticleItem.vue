83751ccaeb01f5aa7656eebe546c03ae2fe9576e
kangcw <kangcw0107@gmail.com>
2024-05-19 21:40:56 +0900

    Feat : 유저 프로필 보기 페이지 컴포넌트 추가

---
+++ nagne-web-vuejs/src/components/User/UserArticleItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="user-article-item" @click="openArticleDetail">
+      {{ userArticle.content }}
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const props = defineProps({
+    userArticle: Object,
+  });
+
+  const openArticleDetail = ()=> {
+    alert(props.userArticle.id)
+  }
+  </script>
+  
+  <style scoped>
+  .user-article-item {
+    width: 250px;
+    height: 250px;
+    background-color: antiquewhite;
+    cursor: pointer;
+  }
+  </style>
+  
