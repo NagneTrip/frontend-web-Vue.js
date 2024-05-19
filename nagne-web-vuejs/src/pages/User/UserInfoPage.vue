60c0ae70dbf966472d65c7e1bd572357441aacad
kangcw <kangcw0107@gmail.com>
2024-05-20 00:31:35 +0900

    Feat : 팔로우/팔로잉 목록 모달 열기/닫기 함수 작성

--- nagne-web-vuejs/src/pages/User/UserInfoPage.vue
+++ nagne-web-vuejs/src/pages/User/UserInfoPage.vue
@@ -6,36 +6,51 @@
             </div>
             <div class="info-box">
                 <div class="info-box-top">
-                    <p class="nickname jua-regular">{{ userInfo.nickname }}</p>
+                    <div class="info-name">
+                        <p class="nickname jua-regular">{{ userInfo.nickname }}</p>
+                        <img :src="`/src/assets/tier/${userInfo.tier}.svg`" :width="25" :height="25" alt=""
+                            class="tier-img" />
+                    </div>
+
                     <button class="edit-profile-btn jua-regular">프로필 편집</button>
+                    <template>
+                        <button class="edit-profile-btn jua-regular">팔로우</button>
+                        <button class="edit-profile-btn jua-regular">팔로잉</button>
+                    </template>
                 </div>
                 <div class="info-box-social">
                     <p class="jua-regular">게시물 {{ userArticlesLen }}</p>
-                    <p class="jua-regular social">팔로워 {{ userInfo.followers }}</p>
-                    <p class="jua-regular social">팔로우 {{ userInfo.followings }}</p>
+                    <p class="jua-regular social" @click="openFollowModal('followers')">팔로워 {{ userInfo.followers }}</p>
+                    <p class="jua-regular social" @click="openFollowModal('following')">팔로잉 {{ userInfo.followings }}
+                    </p>
                 </div>
             </div>
         </div>
         <div class="tab-btn">
             <button class="tab jua-regular" :class="{ 'tab-selected': tabState === 'article' }"
                 @click="changeTab('article')">게시물</button>
-            <button class="tab jua-regular" :class="{ 'tab-selected': tabState === 'plan' }" @click="changeTab('plan')">여행
+            <button class="tab jua-regular" :class="{ 'tab-selected': tabState === 'plan' }"
+                @click="changeTab('plan')">여행
                 계획</button>
         </div>
         <div class="article-section">
             <template v-if="tabState === 'article'">
-                <UserArticle :userArticles="userArticles"/>
+                <UserArticle :userArticles="userArticles" />
             </template>
             <template v-if="tabState === 'plan'">
                 <UserPlan />
             </template>
         </div>
+        <UserFollowing v-if="isFollowingOpen" :userInfo="userInfo" @close-follow-modal="closeFollowModal" />
+        <UserFollowers v-if="isFollowersOpen" :userInfo="userInfo" @close-follow-modal="closeFollowModal" />
     </div>
 </template>
 
 <script setup>
 import UserArticle from '@/components/User/UserArticle.vue';
 import UserPlan from '@/components/User/UserPlan.vue';
+import UserFollowing from '@/components/User/UserFollowing.vue';
+import UserFollowers from '@/components/User/UserFollowers.vue';
 import axios from 'axios';
 import { ref, onMounted, computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
@@ -44,34 +59,54 @@ const route = useRoute();
 const router = useRouter();
 
 const userInfo = ref({});
-const userArticles = ref({});
+const userArticles = ref([]);
 const userIdByParams = route.params.id;
 const tabState = ref('article');
+const isFollowingOpen = ref(false);
+const isFollowersOpen = ref(false);
 
 const changeTab = (tab) => {
     tabState.value = tab;
 }
 
+const openFollowModal = (type) => {
+    switch (type) {
+        case 'followers':
+            isFollowersOpen.value = true;
+            break;
+
+        case 'following':
+            isFollowingOpen.value = true;
+            break;
+    }
+}
+
+const closeFollowModal = () => {
+    isFollowersOpen.value = false;
+    isFollowingOpen.value = false;
+}
+
 onMounted(async () => {
     if (!sessionStorage.getItem('token')) {
         alert('유저 정보는 로그인 후 확인할 수 있습니다.');
-        router.push({ name: 'login' })
+        router.push({ name: 'login' });
         return;
     }
 
-    //유저 정보 로드
+    // 유저 정보 로드
     await axios.get(`http://localhost:8080/api/users/${userIdByParams}`, {
         headers: {
             Authorization: `Bearer ${sessionStorage.getItem('token')}`,
         }
     }).then(({ data }) => {
         userInfo.value = data.response.userInfo;
+        console.log(userInfo.value)
     }).catch(() => {
         alert('유저 정보 로드 실패! 메인으로 돌아갑니다.');
         router.push({ name: 'main' });
     })
 
-    //유저가 작성한 게시물 로드
+    // 유저가 작성한 게시물 로드
     await axios.get(`http://localhost:8080/api/articles/by?userId=${userIdByParams}`, {
         headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}`, }
     }).then(({ data }) => {
@@ -81,7 +116,6 @@ onMounted(async () => {
 const userArticlesLen = computed(() => {
     return userArticles.value.length;
 })
-
 </script>
 
 <style scoped>
@@ -136,6 +170,12 @@ p {
     display: flex;
     justify-content: space-between;
     font-size: 20px;
+}
+
+.info-name {
+    display: flex;
+    align-items: center;
+    gap: 10px;
 }
 
 .nickname {
