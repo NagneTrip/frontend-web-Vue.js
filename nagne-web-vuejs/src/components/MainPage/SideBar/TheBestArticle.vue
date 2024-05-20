b427d924a093477b350f03b14203edf36686fb6a
kangcw <kangcw0107@gmail.com>
2024-05-20 23:58:21 +0900

    Feat : 베스트 게시물 로딩 스피너 추가

--- nagne-web-vuejs/src/components/MainPage/SideBar/TheBestArticle.vue
+++ nagne-web-vuejs/src/components/MainPage/SideBar/TheBestArticle.vue
@@ -1,39 +1,54 @@
 <template>
-    <div class="side-bestarticle">
-      <div class="header">
-        <p class="title jua-regular">베스트 나그네</p>
-        <div class="buttons">
-          <button class="filter"><p class="jua-regular">좋아요순</p></button>
-          <button class="filter"><p class="jua-regular">댓글순</p></button>
-        </div>
-      </div>
-      <div class="article-container">
-        <BestArticleItem v-for="(bestArticle, index) in bestArticles" :bestArticle="bestArticle" :key="bestArticle.id"/>
+  <div class="side-bestarticle">
+    <div class="header">
+      <p class="title jua-regular">베스트 나그네</p>
+      <div class="buttons">
+        <button class="filter">
+          <p class="jua-regular">좋아요순</p>
+        </button>
+        <button class="filter">
+          <p class="jua-regular">댓글순</p>
+        </button>
       </div>
     </div>
+    <div class="article-container">
+      <template v-if="!isLoading">
+        <BestArticleItem v-for="(bestArticle, index) in bestArticles" :bestArticle="bestArticle"
+          :key="bestArticle.id" />
+      </template>
+      <img v-if="isLoading" class="spinner" src="/src/assets/blue_spinner.svg" alt="">
+    </div>
+  </div>
 </template>
 
 <script setup>
 import axios from "axios";
 import BestArticleItem from './BestArticleItem.vue';
-import {ref, onMounted} from 'vue';
-import {useAuthStore} from '@/store/auth.js'
+import { ref, onMounted } from 'vue';
+import { useAuthStore } from '@/store/auth.js'
 const authStore = useAuthStore();
 
 const bestArticles = ref([]);
+const isLoading = ref(false);
 
-onMounted(()=>{
+onMounted(() => {
   getBestArticles();
 })
 
-const getBestArticles = async ()=> {
+const getBestArticles = async () => {
+  isLoading.value = true;
   await axios.get('http://localhost:8080/api/articles/best', {
-    headers : {
+    headers: {
       Authorization: `Bearer ${sessionStorage.getItem('token')}`,
     }
-  }).then(({data})=>{
+  }).then(({ data }) => {
+    isLoading.value = false;
     bestArticles.value = data.response.articles;
-  })
+    
+  }).finally(() => {
+    
+  }
+  )
 }
 
 </script>
@@ -57,6 +72,7 @@ const getBestArticles = async ()=> {
   margin: 0;
   color: rgb(121, 121, 121);
 }
+
 .buttons {
   display: flex;
   gap: 6px;
@@ -68,15 +84,18 @@ const getBestArticles = async ()=> {
   border-radius: 12px;
   border: none;
 }
+
 .filter:hover {
   background-color: rgb(65, 139, 244);
   transition: 0.2s all;
   scale: 1.1;
+
   p {
     color: white;
   }
 }
-.filter p{
+
+.filter p {
   font-size: 12px;
   color: rgb(118, 189, 255);
   margin: 0;
@@ -89,5 +108,10 @@ const getBestArticles = async ()=> {
   flex-direction: column;
   gap: 10px;
   padding-top: 10px;
+}
+.spinner {
+  width: 100px;
+  height: 100px;
+  margin : 80px auto auto auto;
 }
 </style>
