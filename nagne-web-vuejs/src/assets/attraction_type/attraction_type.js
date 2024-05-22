5aaf25b08e03922d283a3e7b06a909f6515af744
monam2 <kangcw0107@gmail.com>
2024-05-23 02:31:20 +0900

    Feat : API 호출 결과에 따라 바인딩할 데이터 객체 생성

---
+++ nagne-web-vuejs/src/assets/attraction_type/attraction_type.js
@@ -0,0 +1,12 @@
+const attraction_type = [
+  { attractionTypeId: 12, name: "관광지", logo: "nature"},
+  { attractionTypeId: 14, name: "문화시설", logo: "culture"},
+  { attractionTypeId: 15, name: "축제/공연/행사", logo: "festival"},
+  { attractionTypeId: 25, name: "여행코스", logo: "trip"},
+  { attractionTypeId: 28, name: "레포츠", logo: "activity"},
+  { attractionTypeId: 32, name: "숙박", logo: "sleep"},
+  { attractionTypeId: 38, name: "쇼핑", logo: "shopping"},
+  { attractionTypeId: 39, name: "음식", logo: "food"},
+];
+
+export {attraction_type};
