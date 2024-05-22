1ad7e4cc653a8b0fc846ae751f5af28d4b8a57aa
monam2 <kangcw0107@gmail.com>
2024-05-22 21:57:58 +0900

    Feat : 검색 결과 페이지 추가

---
+++ nagne-web-vuejs/src/pages/Search/SearchResultPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="card card-body">
+      <div class="search-box">
+        <font-awesome-icon
+          class="serach-icon icon"
+          id="navbar-search-btn"
+          :icon="faMagnifyingGlass"
+        />
+        <input
+          class="search-input jua-regular-large"
+          placeholder="검색어를 입력하세요."
+          type="text"
+        />
+        <button class="jua-regular search-btn">검색</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+</script>
+
+<style scoped></style>
