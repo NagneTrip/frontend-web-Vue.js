f9f2d54ab9e435f8ffa9ead2a4e89193aaa50b3e
monam2 <kangcw0107@gmail.com>
2024-05-24 06:19:22 +0900

    Style : 서버 -> 로컬 API URL 변경

--- nagne-web-vuejs/src/apiClient.js
+++ nagne-web-vuejs/src/apiClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://ec2-54-79-199-126.ap-southeast-2.compute.amazonaws.com:8080',
-  // baseURL: 'http://localhost:8080',
+  // baseURL: 'http://ec2-54-79-199-126.ap-southeast-2.compute.amazonaws.com:8080',
+  baseURL: 'http://localhost:8080',
 });
 
 export default apiClient;
