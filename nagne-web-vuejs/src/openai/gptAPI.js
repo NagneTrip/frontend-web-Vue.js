2a2214337c4ceacf3820bff3e0cb3235c5c3c8f1
monam2 <kangcw0107@gmail.com>
2024-05-24 06:07:57 +0900

    Fix : 프롬프트 수정 - 위도 경도 등 구체적 수치 제거

--- nagne-web-vuejs/src/openai/gptAPI.js
+++ nagne-web-vuejs/src/openai/gptAPI.js
@@ -10,7 +10,8 @@ const getGPTResponse = async (myAttractions, ) => {
 
     const prompt = `${JSON.stringify(myAttractions)}라는 관광지 데이터를 보아놓은 정보가 있어.
     난 이 데이터로 여행을 떠나고 싶은데 어떤 여행지를 먼저 가면 좋을지, 첫 번째 여행지를 다녀온 후 이후의 여행지로는 어딜가면 좋을지 궁금해.
-    내가 준 데이터에서 위도와 경도 좌표를 추출해서 합리적인 여행 계획을 세워줘. 대략적인 소요 시간과 이동 거리도 첨가해서 답변해줘.`;
+    내가 준 데이터에서 위도와 경도 좌표를 추출해서 합리적인 여행 계획을 세워줘. 대략적인 소요 시간과 이동 거리도 첨가해서 답변해줘.
+    답변에 위도 경도는 빼고 대략적인 지역명이나 지리명으로 대답해줘.`;
 
     const response = await openai.chat.completions.create({
       messages: [
