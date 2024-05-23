76201014990986b409674511f90a49fcc17b1924
kangcw0107 <kangcw0107@gmail.com>
2024-05-23 15:54:13 +0900

    Feat : 관광지 관리 pinia store 추가

---
+++ nagne-web-vuejs/src/store/attraction.js
@@ -0,0 +1,20 @@
+// src/store/attraction.js
+import { defineStore } from "pinia";
+import { ref, watch } from "vue";
+
+export const useAttractionStore = defineStore("attraction", () => {
+  const myAttractions = ref([]);
+  const attractionSearchKeyword = ref("");
+
+  const deleteMyAttractions = (attractionId) => {
+    myAttractions.value = myAttractions.value.filter(
+      (attraction) => attraction.id !== attractionId
+    );
+  };
+
+  const changeKeyword = (newValue) => {
+    attractionSearchKeyword.value = newValue;
+  };
+
+  return { myAttractions, deleteMyAttractions, changeKeyword };
+});
